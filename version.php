230c7ccf3fe1d2e7b818837beccbcf8d339031a5
Leon Berau <leon.berau@ruhr-uni-bochum.de>
2024-10-15 16:22:23 +0200

    Border primary variable

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_simple_calculator';
 $plugin->release = 'v4.5-r1';
-$plugin->version = 2024080601;
+$plugin->version = 2024080602;
 $plugin->requires = 2022112800;
 $plugin->supported = [403, 405];
 $plugin->maturity = MATURITY_RC;
