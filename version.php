9878cbaed880d7f3de368339d6547f273f05bee2
Ruhr-University Bochum <75328238+rub-moodle-its@users.noreply.github.com>
2026-01-13 09:57:00 +0100

    Update version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_calculator';
-$plugin->release = 'v5.0.3-r2';
+$plugin->release = 'v5.0.1-r2';
 $plugin->version = 2025071002;
 $plugin->requires = 2022112800;
-$plugin->supported = [403, 503];
+$plugin->supported = [403, 501];
 $plugin->maturity = MATURITY_RC;
